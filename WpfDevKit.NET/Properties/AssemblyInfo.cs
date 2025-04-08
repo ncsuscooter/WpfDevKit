6b3a2e102982715a4356897146c0a8a2219b801f
ncsuscooter <ncsuscooter@gmail.com>
2025-04-07 22:39:30 -0400

    tiny

--- WpfDevKit.NET/Properties/AssemblyInfo.cs
+++ WpfDevKit.NET/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A comprehensive starter kit for WPF applications, providing a solid foundation built around the MVVM design pattern. This toolkit offers reusable components, utilities, and best practices to accelerate development, improve maintainability, and streamline the creation of modern desktop applications using WPF and .NET Framework 4.6.1.")]
 [assembly: AssemblyConfiguration("Development")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfDevKit")]
+[assembly: AssemblyProduct("WpfDevKit.NET")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
