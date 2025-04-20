02e50bf3d5ea6b52e56aa896cb20a46859d8caa3
ncsuscooter <ncsuscooter@gmail.com>
2025-04-20 16:09:17 -0400

    1

---
+++ WpfDevKit.UI/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using WpfDevKit.DependencyInjection;
+using WpfDevKit.UI.CollectionSynchronization;
+using WpfDevKit.UI.Command;
+using WpfDevKit.UI.ContextSynchronization;
+using WpfDevKit.UI.Dialogs;
+
+namespace WpfDevKit.UI
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddUIServices<TMainViewModel>(this IServiceCollection services) where TMainViewModel : class => 
+            services.AddCollectionSynchronization()
+                    .AddCommandFactory()
+                    .AddContextSynchronization()
+                    .AddDialogService()
+                    .AddSingleton<TMainViewModel>();
+    }
+}
