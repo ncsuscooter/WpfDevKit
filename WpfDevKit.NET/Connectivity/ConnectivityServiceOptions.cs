0cf28fd41c81bef289873fe624dc07c624807096
ncsuscooter <ncsuscooter@gmail.com>
2025-04-24 13:16:33 -0400

    34

--- WpfDevKit.NET/Connectivity/ConnectivityServiceOptions.cs
+++ WpfDevKit.NET/Connectivity/ConnectivityServiceOptions.cs
@@ -63,7 +63,7 @@ namespace WpfDevKit.Connectivity
         public Func<IConnectivityService, string> GetStatus { get; set; } = 
             x => x.State == TConnectivityState.Connecting ? "connecting" :
             x.State == TConnectivityState.Connected ? "connected" :
-            $"retrying in {DateTime.Now.Subtract(x.NextAttempt).ToReadableTime()} after [{x.Attempts}] failed attempts";
+            $"retrying in {x.NextAttempt.Subtract(DateTime.UtcNow).ToReadableTime()} after [{x.Attempts}] failed attempts";
 
         /// <summary>
         /// Validates the current configuration and throws an exception if any required properties are missing or invalid.
